7f7b2769653f8ba04389d18deb6090e81262212e
Adalberto Ferreira <adalbertoferreiradasilvaneto@gmail.com>
2019-05-03 00:31:34 -0300

    New migration and column name fix.

--- backend/ConsultingManager.Infra/Database/Models/ProcessPoco.cs
+++ backend/ConsultingManager.Infra/Database/Models/ProcessPoco.cs
@@ -5,7 +5,7 @@ namespace ConsultingManager.Infra.Database.Models
     public class ProcessPoco
     {
         public Guid Id { get; set; }
-        public string Descritpion { get; set; }
+        public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EstimatedEndDate { get; set; }
         public DateTime EndDate { get; set; }
