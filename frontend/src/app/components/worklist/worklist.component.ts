85cab52e27d8592ee52c0952c8dc083cc89195f8
Adalberto Ferreira <adalbertoferreiradasilvaneto@gmail.com>
2019-06-21 20:00:32 -0300

    Worklist - Added filter by customer

--- frontend/src/app/components/worklist/worklist.component.ts
+++ frontend/src/app/components/worklist/worklist.component.ts
@@ -7,61 +7,86 @@ import { Customer } from 'src/app/models/customer.model';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
-    selector: 'app-worklist',
-    templateUrl: './worklist.component.html',
-    styleUrls: ['./worklist.component.css']
+  selector: 'app-worklist',
+  templateUrl: './worklist.component.html',
+  styleUrls: ['./worklist.component.css']
 })
 export class WorklistComponent implements OnInit {
 
-    constructor(private taskService: TaskService,
-        private userService: UserService,
-        private route: ActivatedRoute,
-        private router: Router) { }
+  constructor(private taskService: TaskService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
-    public tasksList: Task[];
-    public loggedUser: User;
-    
-    ngOnInit() {
+  public tasksList: Task[];
+  public filteredTasksList: Task[] = [];
+  public loggedUser: User;
+  public customersList: string[] = [];
 
-        this.loggedUser = this.userService.getUser();
+  ngOnInit() {
 
-        if (!this.loggedUser) {
-            this.router.navigate(['login']);
+    this.loggedUser = this.userService.getUser();
+
+    if (!this.loggedUser) {
+      this.router.navigate(['login']);
+    }
+
+    this.loadTasks();
+  }
+
+  async loadTasks() {
+    this.tasksList = await this.taskService.getUserTasks(this.loggedUser.id);
+    this.filteredTasksList = this.tasksList;
+    this.loadCustomersList();
+  }
+
+  async loadCustomersList() {
+    debugger;
+    if (this.tasksList && this.tasksList.length > 0) {
+      for (let i = 0; i < this.tasksList.length; i++) {
+        let task = this.tasksList[i];
+        if (this.customersList.indexOf(task.customer.name) < 0) {
+          this.customersList.push(task.customer.name);
+          this.customersList.sort((a, b) => a.localeCompare(b));
         }
-        
-        this.loadTasks();
+      }
     }
+  }
 
-    async loadTasks() {
-        this.tasksList = await this.taskService.getUserTasks(this.loggedUser.id);
+  updateSelectedTask(task: Task, event: Event) {
+    event.preventDefault();
+    this.router.navigate(['task'])
+    window.localStorage.setItem('task', JSON.stringify(task));
+  }
+
+  updateSelectedCustomer(customer: Customer, event: Event) {
+    event.preventDefault();
+    window.localStorage.setItem("customer", JSON.stringify(customer));
+    this.router.navigate(['timeline']);
+  }
+
+  loadClassIndicator(task: Task) {
+    let today: Date = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    if (!task.endDate) {
+      if (today > new Date(task.estimatedEndDate)) {
+        return "indicator label-danger";
+      } else if (today.getTime() >= new Date(task.startDate).getTime()) {
+        return "indicator label-warning";
+      } else {
+        return "indicator label-default";
+      }
+    } else {
+      return "indicator label-success";
     }
+  }
 
-    updateSelectedTask(task: Task, event: Event) {
-        event.preventDefault();
-        this.router.navigate(['task'])
-        window.localStorage.setItem('task', JSON.stringify(task));
+  onCustomerChange(customerName: string) {
+    if (customerName == '') {
+      this.filteredTasksList = this.tasksList;
+    } else {
+      this.filteredTasksList = this.tasksList.filter(task => task.customer.name == customerName);
     }
-
-    updateSelectedCustomer(customer: Customer, event: Event) {
-        event.preventDefault();
-        window.localStorage.setItem("customer", JSON.stringify(customer));
-        this.router.navigate(['timeline']);        
-    }
-
-    loadClassIndicator(task: Task) {
-        let today: Date = new Date();
-        today.setHours(0, 0, 0, 0);
-
-        if (!task.endDate) {
-            if (today > new Date(task.estimatedEndDate)) {
-                return "indicator label-danger";
-            } else if (today.getTime() >= new Date(task.startDate).getTime()) {
-                return "indicator label-warning";
-            } else {
-                return "indicator label-default";
-            }
-        } else {
-            return "indicator label-success";
-        }
-    }
+  }
 }
