d7f05cac0e4a937f27b914122a02bde4f2535319
Adalberto Ferreira <adalbertoferreiradasilvaneto@gmail.com>
2019-08-20 18:12:58 -0300

    [Flightplan] Finished the first version of the feature

--- frontend/src/app/components/flightplan/flightplan.component.ts
+++ frontend/src/app/components/flightplan/flightplan.component.ts
@@ -4,6 +4,11 @@ import { CustomersService } from 'src/app/services/customers/customers.service';
 import { Customer } from 'src/app/models/customer.model';
 import { CustomerMeeting } from 'src/app/models/customermeeting.model';
 import { CustomerProcess } from 'src/app/models/customerprocess.model';
+import { NgbModalRef, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ProcessService } from 'src/app/services/process/process.service';
+import { ModelProcess } from 'src/app/models/modelprocess.model';
+import { UserService } from 'src/app/services/user/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-flightplan',
@@ -12,18 +17,33 @@ import { CustomerProcess } from 'src/app/models/customerprocess.model';
 })
 export class FlightplanComponent implements OnInit {
 
+  public loggedUser: User;
   constructor(private router: Router,
-    private customersService: CustomersService) { }
+    private userService: UserService,
+    private processService: ProcessService,
+    private customersService: CustomersService,
+    private modalService: NgbModal) {
+
+    this.loggedUser = this.userService.getUser();
+
+    if (!this.loggedUser) {
+      this.router.navigate(['login']);
+    }
+  }
 
   public customer: Customer;
   public meetings: CustomerMeeting[] = [];
-  public processes: CustomerProcess[] = []
+  public customerProcesses: CustomerProcess[] = []
+  public filteredCustomerProcesses: CustomerProcess[] = []
+  public modelProcessesList: ModelProcess[] = [];
   public selectedMeeting: CustomerMeeting;
-  
+  public modalObject: NgbModalRef;
 
   ngOnInit() {
     this.loadCustomer();
     this.loadMeetings();
+    this.loadCustomerProcesses();
+    this.loadModelProcesses();
   }
 
   loadCustomer() {
@@ -32,15 +52,30 @@ export class FlightplanComponent implements OnInit {
     }
   }
 
+  async loadModelProcesses() {
+    this.modelProcessesList = await this.processService.getModelProcesses();
+  }
+
+  async loadCustomerProcesses() {
+    this.customerProcesses = await this.processService.getCustomerProcesses(this.customer.id);
+    this.filteredCustomerProcesses = this.customerProcesses;
+  }
+
   async loadMeetings() {
     this.meetings = await this.customersService.getMeetings(this.customer.id);
-
-    if (this.meetings.length > 0) {
-      this.selectedMeeting = this.meetings[0];
-    }
   }
 
   selectMeeting(meeting: CustomerMeeting) {
     this.selectedMeeting = meeting;
+    this.filteredCustomerProcesses = this.customerProcesses.filter(o => o.customerMeetingId == this.selectedMeeting.id);
+  }
+
+  openProcessModal(content) {
+    this.modalObject = this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
+  }
+
+  async addProcess(modelProcessId: string, modelDescription: string, customerUserId: string, startDate: string) {
+    await this.processService.startCustomerProcess(modelProcessId, modelDescription, this.customer.id, this.loggedUser.id, customerUserId, new Date(startDate), this.selectedMeeting.id);
+    this.modalObject.close();
   }
 }
